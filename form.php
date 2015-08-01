5f93f6e0c47b05c302eb5138f83dcbf2a724e737
damasterang <damasterang@outlook.com>
2015-08-01 09:30:24 +0700

    ahaiiiii

---
+++ form.php
@@ -0,0 +1,5 @@
+<form action="submit.php" method="post"> 
+Username <input type="text" name="username">
+<input type="submit" name="submit" 
+value="Submit"> 
+</form> 
---
+++ hal2.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+echo "Ini adalah halaman 2<br>"; 
+echo "Nama user Anda adalah: ".$_SESSION
+['Nama']."<br><br>"; 
+echo  "<a  href=hal1.php>Hal  1</a>  <a  href=hal2.php>Hal  2</a>  <a 
+href=hal3.php>Hal 3</a>"; 
+?> 
